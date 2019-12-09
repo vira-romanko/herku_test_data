02148b20b71a62b23d48a20b694d7e64b079a737
Vira <vira.romanko99@gmail.com>
2019-12-09 08:52:36 -0500

    config2

--- utils/sqlConnect.js
+++ utils/sqlConnect.js
@@ -1,15 +1,26 @@
-var mysql = require('mysql');
-var config = require('../config');
+// var mysql = require('mysql');
+// var config = require('../config');
 
-var connect = mysql.createPool({
-  host: config.host,
-  port: config.port,
-  user: config.user,
-  password: config.password,
-  database: config.database,
-  connectionLimit : 20,
-  queueLimit : 100,
-  waitForConnections : true
-});
+const config = {
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  port: 3306, 
+  uname: "b0df3641b70d8b",
+  pword: "f622741a", 
+  database: "heroku_d3bc50f9027c606?"
+}
 
-module.exports = connect;
+module.exports = config;
+
+
+// var connect = mysql.createPool({
+//   host: config.host,
+//   port: config.port,
+//   user: config.user,
+//   password: config.password,
+//   database: config.database,
+//   connectionLimit : 20,
+//   queueLimit : 100,
+//   waitForConnections : true
+// });
+
+// module.exports = connect;
