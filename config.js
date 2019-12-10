cefc84a587077e7b1eb03c7f94361a5a9f8e9baa
Vira <vira.romanko99@gmail.com>
2019-12-10 14:58:24 -0500

    more fixes

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     host: "us-cdbr-iron-east-05.cleardb.net",
   
-    uname: "b0df3641b70d8b",
-    pword: "f622741a", 
+    username: "b0df3641b70d8b",
+    password: "f622741a", 
     database: "heroku_d3bc50f9027c606?"
   }
   
