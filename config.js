0ee183ea9a4e64e5c71912eb79d9848b9d66a691
Vira Romanko <43069942+vira-romanko@users.noreply.github.com>
2019-12-10 15:05:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   
     user: "b0df3641b70d8b",
     password: "f622741a", 
-    database: "heroku_d3bc50f9027c606?"
+    database: "heroku_d3bc50f9027c606"
   }
   
   module.exports = config;
