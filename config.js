fa841f599249c295ab8505d6453b56a418ca2d25
Vira Romanko <43069942+vira-romanko@users.noreply.github.com>
2019-12-10 15:13:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     host: "us-cdbr-iron-east-05.cleardb.net",
   
-    user: "b0df3641b70d8b",
-    password: "f622741a", 
+    uname: "b0df3641b70d8b",
+    pname: "f622741a", 
     database: "heroku_d3bc50f9027c606"
   }
   
